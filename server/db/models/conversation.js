759578136ffd290552dc022d0b55433b89b4f2ea
Umang Patel <umangpatel1991@gmail.com>
2021-12-13 21:48:52 -0500

    alter conversation model to add unreads column

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,8 +1,15 @@
+const Sequelize = require("sequelize");
 const { Op } = require("sequelize");
 const db = require("../db");
 const Message = require("./message");
 
-const Conversation = db.define("conversation", {});
+const Conversation = db.define("conversation", {
+  unreads: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  }
+});
 
 // find conversation given two user Ids
 
